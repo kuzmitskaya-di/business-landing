c71ffd3028f539b39197e898d66758315608acaa
diana_kuz <kuzmitskaya.di@gmail.com>
2020-11-02 19:24:43 +0300

    fix - changed the position of the blocks;
    - wrapper and wrapperForm.

--- js/validation.js
+++ js/validation.js
@@ -2,21 +2,19 @@ function validation(){
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let error_message = document.getElementById("error_message");
+    let error_message2 = document.getElementById("error_message2");
     let text;
 
     error_message.style.padding = "10px";
+    error_message2.style.padding = "10px";
 
     if(name.length < 5){
         text = "Имя - 5-15 букв!";
-        error_message.innerHTML = text;
+        error_message2.innerHTML = text;
         return false;
     }
-    if(name.length > 15){
-        text = "Имя - 5-15 букв!";
-        error_message.innerHTML = text;
-        return false;
-    }
-    if(email.indexOf("@")){
+
+    if (email.indexOf("@")){
         text = "Введите Email используя - @";
         error_message.innerHTML = text;
         return false;
