c306b03ac29b1195c1b5f346b96243885a19498e
diana_kuz <kuzmitskaya.di@gmail.com>
2020-11-04 22:12:05 +0300

    Fix - new js code for select.js.

--- js/validation.js
+++ js/validation.js
@@ -1,23 +1,27 @@
-function validation(){
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let error_message = document.getElementById("error_message");
-    let error_message2 = document.getElementById("error_message2");
-    let text;
+(function() {
+        (".validateBtn").on("click", validate);
 
-    error_message.style.padding = "10px";
-    error_message2.style.padding = "10px";
+        // Validate email
+        function validateEmail(email) {
+                let re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+                return re.test(String(email).toLowerCase());
+        }
+        function sendForm() {
+                (".error_email").text("Данные введены верно").fadeIn();
+        }
+        function validate() {
+                let email = (".form_email").val();
+                let error = (".error_email");
+                error.text("");
 
-    if(name.length < 5){
-        text = "Имя - 5-15 букв!";
-        error_message2.innerHTML = text;
-        return false;
-    }
+                if (validateEmail(email)) {
+                        error.fade();
+                        sendForm();
+                } else {
+                        error.fadeIn();
+                        error.text(" Email введён не верно!");
+                }
+                return false;
+        }
+});
 
-    if (email.indexOf("@")){
-        text = "Введите Email используя - @";
-        error_message.innerHTML = text;
-        return false;
-    }
-
-}
