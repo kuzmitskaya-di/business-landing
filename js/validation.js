588713de669205ca6bd3434fb5978f10a751a9ac
diana_kuz <kuzmitskaya.di@gmail.com>
2020-11-05 19:50:18 +0300

    Fix - new js code for validation.js.

--- js/validation.js
+++ js/validation.js
@@ -3,6 +3,17 @@ let inputs = document.querySelectorAll("input[data-rule]");
 for (input of inputs) {
         input.addEventListener("blur", function () {
                 let rule = this.dataset.rule;
-                console.log(rule);
+                let value = this.value;
+                let check;
+
+                switch (rule) {
+                        case "email":
+
+                        break;
+                }
+
+                if (check) {
+                        document.getElementsByTagName("input").add.style.border = "1px solid green";
+                }
         });
 }
