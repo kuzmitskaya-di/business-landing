403d86dee5a20f672cddfec8df94f9e05e5ec383
Diana <kuzmitskaya.di@gmail.com>
2020-11-09 00:05:55 +0300

    fix - all the selectors.

--- js/select.js
+++ js/select.js
@@ -1,33 +1,26 @@
+function select() {
+    const select = document.querySelector(".select");
+    const selectItem = document.querySelectorAll(".select__item");
 
-
-    let select = function () {
-        let selectHeader = document.querySelectorAll(".select__header");
-        let selectItem = document.querySelectorAll(".select__item");
-
-
-        selectHeader.forEach(item => {
-            item.addEventListener("click", selectToggle)
-        })
-
+    select.addEventListener("click", (event) => {
+        selectToggle();
         selectItem.forEach(item => {
-            item.addEventListener("click", selectChoose)
+            if (item === event.target) {
+                selectChoose(item);
+            }
         })
+    });
 
+    function selectToggle() {
+        select.classList.toggle("is-active")
+    }
 
-        function selectToggle() {
-            this.parentElement.classList.toggle("is-active")
-        }
-
-        function selectChoose() {
-            let text = this.innerText,
-                select = this.closest(".select"),
-                currentText = select.querySelector(".select__current");
-            currentText.innerText = text;
-            select.classList.remove("is-active");
-        }
-    };
+    function selectChoose(item) {
+        const currentText = item.closest(".select").querySelector(".select__current");
+        currentText.innerText = item.innerText;
+    }
+}
 
 select();
-
 
 
--- js/validation.js
+++ js/validation.js
@@ -1,19 +1,1 @@
-let inputs = document.querySelectorAll("input[data-rule]");
 
-for (input of inputs) {
-        input.addEventListener("blur", function () {
-                let rule = this.dataset.rule;
-                let value = this.value;
-                let check;
-
-                switch (rule) {
-                        case "email":
-
-                        break;
-                }
-
-                if (check) {
-                        document.getElementsByTagName("input").add.style.border = "1px solid green";
-                }
-        });
-}
