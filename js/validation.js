25136557e4536851c70cffe86f2c01edf38e2eb0
Diana <kuzmitskaya.di@gmail.com>
2020-11-17 02:52:02 +0300

    fix - validation.js file:
    - shortened the code.

--- js/validation.js
+++ js/validation.js
@@ -1,25 +1,26 @@
-let inputs = document.querySelectorAll("input[data-rule]");
+const inputs = document.querySelectorAll("input[data-rule]");
+
+function valid(param) {
+    return document.forms["form-input"].elements[param].value;
+}
 
 for (let input of inputs) {
-    input.addEventListener("blur", function () {
-        let rule = this.dataset.rule;
-
-
+    window.onload = input.addEventListener("blur", function () {
+        const rule = this.dataset.rule;
 
         switch (rule) {
 
             case "email":
-            case "name":
-                function valid(param) {
-                    return document.forms["form-input"].elements[param].value;
-                }
                 const regEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
                 if (regEmail.test(valid("email")) === false) {
                     alert("Введите корректный e-mail!");
                     return false;
                 }
+                break;
+            case "name":
                 const regName = /^[A-Za-z]+$/;
-                if (regName.test(valid("name")) === false && valid("name").length >=5 && valid("name").length <=15 ) {
+                let long = (valid("name")).length;
+                if (regName.test(valid("name")) === false || long >= 5 || long <= 15) {
                     alert("Введите корректное имя!");
                     return false;
                 }
