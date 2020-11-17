f1cab16f572e4f0c4a9ec1842cfc8fd7f39c2000
diana_kuz <kuzmitskaya.di@gmail.com>
2020-11-17 22:19:35 +0300

    fix - select.js file:
    - added new method.

--- js/select.js
+++ js/select.js
@@ -1,28 +1,31 @@
+function select() {
+    const select = document.querySelector(".select");
+    const selectItem = document.querySelectorAll(".select__item");
 
- function select() {
-        const select = document.querySelector(".select");
-        const selectItem = document.querySelectorAll(".select__item");
+    select.addEventListener("click", (event) => {
+        selectToggle();
+        selectItem.forEach(item => {
+            if (item === event.target) {
+                selectChoose(item);
+            }
+        })
+    });
 
-        select.addEventListener("click", (event) => {
-            selectToggle();
-            selectItem.forEach(item => {
-                if (item === event.target) {
-                    selectChoose(item);
-                }
-            })
-        });
-
-        function selectToggle() {
-            select.classList.toggle("is-active")
-        }
-
-        function selectChoose(item) {
-            const currentText = item.closest(".select").querySelector(".select__current");
-            currentText.innerText = item.innerText;
-        }
+    function selectToggle() {
+        select.classList.toggle("is-active")
     }
 
- select();
+    function selectChoose(item) {
+        const currentText = item.closest(".select").querySelector(".select__current");
+        currentText.innerText = item.innerText;
+    }
+}
+
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", select);
+} else {
+    select();
+}
 
 
 
