5c625c6cf3ace8bd9e231f4f8e0279cafb9a68d7
Diana <kuzmitskaya.di@gmail.com>
2020-11-17 13:17:41 +0300

    fix - validation.js file and responsive.

--- js/validation.js
+++ js/validation.js
@@ -1,31 +1,34 @@
-const inputs = document.querySelectorAll("input[data-rule]");
+window.onload = function validation() {
 
-function valid(param) {
-    return document.forms["form-input"].elements[param].value;
-}
+    const inputs = document.querySelectorAll("input[data-rule]");
 
-for (let input of inputs) {
-    window.onload = input.addEventListener("blur", function () {
-        const rule = this.dataset.rule;
+    function valid(param) {
+        return document.forms["form-input"].elements[param].value;
+    }
 
-        switch (rule) {
+    for (let input of inputs) {
+        input.addEventListener("blur", function () {
+            const rule = this.dataset.rule;
 
-            case "email":
-                const regEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-                if (regEmail.test(valid("email")) === false) {
-                    alert("Введите корректный e-mail!");
-                    return false;
-                }
-                break;
-            case "name":
-                const regName = /^[A-Za-z]+$/;
-                let long = (valid("name")).length;
-                if (regName.test(valid("name")) === false || long >= 5 || long <= 15) {
-                    alert("Введите корректное имя!");
-                    return false;
-                }
-                break;
-        }
+            switch (rule) {
 
-    })
+                case "email":
+                    const regEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+                    if (regEmail.test(valid("email")) == false) {
+                        alert("Введите корректный e-mail!");
+                        return false;
+                    }
+                    break;
+                case "name":
+                    const regName = /^[A-Za-z]+$/;
+                    let long = (valid("name"));
+                    if (regName.test(long) == false || long.length >= 5 || long.length <= 15) {
+                        alert("Введите корректное имя!");
+                        return false;
+                    }
+                    break;
+            }
+
+        })
+    }
 }
