03d70cb377d11334c12d94f7384ee25f213bcda4
diana_kuz <kuzmitskaya.di@gmail.com>
2020-11-17 21:49:16 +0300

    fix - select.js file.

--- js/select.js
+++ js/select.js
@@ -1,25 +1,29 @@
-window.onload = function select() {
-    const select = document.querySelector(".select");
-    const selectItem = document.querySelectorAll(".select__item");
 
-    select.addEventListener("click", (event) => {
-        selectToggle();
-        selectItem.forEach(item => {
-            if (item === event.target) {
-                selectChoose(item);
-            }
-        })
-    });
+ function select() {
+        const select = document.querySelector(".select");
+        const selectItem = document.querySelectorAll(".select__item");
 
-    function selectToggle() {
-        select.classList.toggle("is-active")
+        select.addEventListener("click", (event) => {
+            selectToggle();
+            selectItem.forEach(item => {
+                if (item === event.target) {
+                    selectChoose(item);
+                }
+            })
+        });
+
+        function selectToggle() {
+            select.classList.toggle("is-active")
+        }
+
+        function selectChoose(item) {
+            const currentText = item.closest(".select").querySelector(".select__current");
+            currentText.innerText = item.innerText;
+        }
     }
 
-    function selectChoose(item) {
-        const currentText = item.closest(".select").querySelector(".select__current");
-        currentText.innerText = item.innerText;
-    }
-}
+ select();
+
 
 
 
